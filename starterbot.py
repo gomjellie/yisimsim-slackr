fd35d07d8590ab104eb637d271ac7715f170a329
gomjellie <gomjellie@naver.com>
2016-10-29 00:36:04 +0900

    작동은하는데 실행할때마다 예전 db를 날려먹음 ㅋ

--- starterbot.py
+++ starterbot.py
@@ -16,25 +16,49 @@ con = sqlite3.connect("chat.db")
 print("chat.db created !")
 cursor = con.cursor()
 print("cursor created !")
-cursor.execute("CREATE TABLE chatlog(Quest text, Ans text)")
+#cursor.execute("CREATE TABLE chatlog(Quest text, Ans text)")
+isLearning = False
+prevQuest = ''
 
 def handle_command(command, channel):
+    global con
+    global isLearning
+    global cursor
+    global prevQuest
     """
         Receives commands directed at the bot and determines if they
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    cursor = con.execute("SELECT Quest, Ans FROM chatlog WHERE Quest like \'%%" + command + "%%\' ORDER BY RANDOM() LIMIT 1")
-    response = cursor.fetchone()
-    if not response:
-    	cursor.execute("INSERT INTO chatlog VALUES('" + command + "', 'what can i say to that?')")
-    else:
-    	response = response[1]
+    if not isLearning:
+        print("if not isLearning")
+        cursor = con.execute("SELECT * FROM chatlog WHERE Quest=?", (command,))
+        response = cursor.fetchone()
+        if not response:
+            print("if not response")
 
-    #if command.startswith(EXAMPLE_COMMAND):
-        #response = "뭐하라고?"
-    slack_client.api_call("chat.postMessage", channel=channel,
-            text=response, as_user=True)
+            #cursor.execute("INSERT INTO chatlog VALUES('{}', 'what can i say to that?')".format(command))
+            #cursor.execute("INSERT INTO chatlog VALUES('" + command + "', 'what can i say to that?')")
+            prevQuest = command
+            response = 'what can i say to that?'
+            isLearning = True
+        else:
+            print("if response")
+            response = response[1]
+            isLearning = False;
+
+        #if command.startswith(EXAMPLE_COMMAND):
+            #response = "뭐하라고?"
+        
+    else: #if isLearning == True
+    	print("if isLearning")
+    	t = (prevQuest, command)
+    	cursor.execute("INSERT INTO chatlog VALUES (?,?)", t)
+    	response = "i've learned well"
+    	isLearning = False;
+    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+    
+
 
 
 def parse_slack_output(slack_rtm_output):
