af9ec459864041d8e387fe0d6b9090084c0b6145
gomjellie <gomjellie@naver.com>
2016-10-29 22:38:43 +0900

    gitignore

--- exportSettings.py
+++ exportSettings.py
@@ -1,5 +1,5 @@
 import os
-os.environ["SLACK_BOT_TOKEN"] = "secret"
-os.environ["BOT_ID"] = "secret"
+os.environ["SLACK_BOT_TOKEN"] = "xoxb-96451072629-mtFMomOOWfWjcC37I6bVW0om"
+os.environ["BOT_ID"] = "U2UD924JH"
 
 #import starterbot
