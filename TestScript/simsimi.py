99cf4b351e7041d6940462724b9c6c12d8f106cd
karlin13 <karlin@localhost.localdomain>
2016-11-07 20:43:25 +0900

    안쓰는 파일 정리 및 starterbot.py를 yisimsimbot.py으로 변경

---
+++ TestScript/simsimi.py
@@ -0,0 +1,17 @@
+import requests
+
+values = {
+    'key':'037def59-fda0-46eb-93f4-9fce096f3528',
+    'lc': 'en',
+    'ft': '1.0',
+    'text': 'your TEXT HERE'
+}
+
+key = '037def59-fda0-46eb-93f4-9fce096f3528'
+query = 'http://sandbox.api.simsimi.com/request.p?key=' + key + '&lc=en&ft=1.0&text='
+url = 'http://sandbox.api.simsimi.com/request.p'
+
+r= requests.get(query + 'do you know kimchi?')
+values['text'] = '뭐해?'
+r = requests.get(url, params = values)
+print(r.json().get('response'))
