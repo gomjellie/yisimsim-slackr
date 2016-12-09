94e3a94a4aef005091c970424da6cf1c977c7d12
Oh InQueue <gomjellie@naver.com>
2016-12-09 18:36:16 +0900

    Update exportSettings.py

--- exportSettings.py
+++ exportSettings.py
@@ -1,5 +1,5 @@
 import os
-os.environ["SLACK_BOT_TOKEN"] = "xoxb-96451072629-mtFMomOOWfWjcC37I6bVW0om"#your token here
+os.environ["SLACK_BOT_TOKEN"] = "secret"#your token here
 os.environ["BOT_ID"] = "U2UD924JH"#your bot_id here
 
 #import starterbot
